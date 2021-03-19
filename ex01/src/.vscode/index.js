872208115a2aae855e3193719572d6ce756351a5
Dami-art <damjana.zoric@gmail.com>
2021-03-19 07:03:48 +0100

    new ex

---
+++ ex01/src/.vscode/index.js
@@ -0,0 +1,11 @@
+//create an array here
+varr myArray [2, 4, 0, 8, 10];
+//end of creating an array
+function myArrayFunction(arr) {
+var myItems = [ ...arr];
+//code below
+return myItems
+//code under
+}
+console.log(myArrayFunction(myItems));
+module.exports = myArrayFunction;
---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+
+//create temps arr here
+var temps = [
+    var monday = [31, 32, 19, 37];
+    var tuesday = [29, 27, 55, 36];
+    var wednesday = [17, 27, 42, 46];
+    var thursday = [29, 52, 21, 64];
+    var friday = [91, 27, 31, 61];
+];
+//end of temps arr
+function myArrayFunction(arr) {
+var newTemps = [...arr];
+var averageDayTemp = [];
+//change code below
+return averageDayTemp;
+}
+console.log(myArrayFunction());
+module.exports = myArrayFunction;
